9d513ea9ae404acd6e96b0f6b8fa6549ca54b24b
hannah melendy, Jake Nagel and Nathan Aden <nathan@>
2019-03-28 10:24:34 -0700

    add age verification logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   $('#gate-keeper').modal({backdrop: 'static', keyboard: false});
+  $("#age-submit").click(function(){
+    console.log ($('#age-varification').is(":checked"));
+  });
 
-
-  $("#gate-keeper").modal('hide');
-  $("#gate-keeper").effect("shake");
+  // $("#gate-keeper").modal('hide');
+  // $("#gate-keeper").effect("shake");
 
 });
