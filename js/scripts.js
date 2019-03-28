4e284116f594c00f84cdfb82042817cf1ca9251e
hannah melendy, Jake Nagel and Nathan Aden <nathan@>
2019-03-28 10:38:51 -0700

    check for age or shake box

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
   $('#gate-keeper').modal({backdrop: 'static', keyboard: false});
   $("#age-submit").click(function(){
-    console.log ($('#age-varification').is(":checked"));
+    var ageChecker = $('#age-varification').is(":checked");
+
+    if(ageChecker === true) {
+      $("#gate-keeper").modal('hide');
+    } else {
+      $("#gate-keeper").effect("shake");
+    }
+
+
+
+
+
+
+
+
+
   });
-
-  // $("#gate-keeper").modal('hide');
-  // $("#gate-keeper").effect("shake");
-
 });
